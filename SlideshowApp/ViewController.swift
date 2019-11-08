b12416b2965fec2d9bfe4243cff988a9f14f3466
natsuya yamashiro <azuki728@me.com>
2019-11-08 18:27:46 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by NATSUYA YAMASHIRO on 2019/11/08.
+//  Copyright © 2019 natsuya.yamashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
